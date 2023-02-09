b5dd53849d7c0d4cf601c2521ec842256ee44a66
dsaint31 <dsaint31@gmail.com>
2023-02-09 09:11:14 +0900

    add test

--- ch01/empty_window_ps.py
+++ ch01/empty_window_ps.py
@@ -1,24 +1,36 @@
 # basic_window.py
 # Import necessary modules
 import sys
-import PySide6.QtCore
-from PySide6.QtWidgets import QApplication, QWidget
+# import PySide6.QtCore
+# from PySide6.QtWidgets import (QApplication, QWidget,
+#                                QLabel)
+import PyQt6.QtCore
+from PyQt6.QtWidgets import (QApplication, QWidget,
+                               QLabel)
 
-class EmptyWindow(QWidget):
+class MW(QWidget):
     def __init__(self):
         """ Constructor for Empty Window Class """
         super().__init__()
         self.initializeUI()
     def initializeUI(self):
-        """Set up the application."""
-        self.setGeometry(200, 100, 400, 300)
-        self.setWindowTitle("Empty Window in PyQt")
+        """set up the application."""
+        self.setGeometry(200, 100, 400, 200)
+        self.setWindowTitle("Main Window in PyQt")
+        self.setup_main_wnd()
         self.show() # Display the window on the screen
+        
+    def setup_main_wnd(self):
+        """set up the main window."""
+        hello_label = QLabel(self)
+        hello_label.setText('Hello, World and Qt!')
+        hello_label.move(150,90)
 
 # Run the program
 if __name__ == '__main__':
-    print(PySide6.__version__)
-    print(PySide6.QtCore.__version__)
+    # print(PySide6.__version__)
+    # print(PySide6.QtCore.__version__)
+    print(PyQt6.QtCore.qVersion())
     app = QApplication(sys.argv)
-    window = EmptyWindow()
+    window = MW()
     sys.exit(app.exec())
