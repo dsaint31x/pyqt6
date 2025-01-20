a2da6f63bd3d164d7d6cea7ff92e8e9fede1100f
dsaint31 <dsaint31@gmail.com>
2025-01-20 09:07:17 +0900

    fix ch01

---
+++ ch01/first_custom_dlg.py
@@ -0,0 +1,57 @@
+import sys
+from PySide6.QtWidgets import ( QApplication,
+    QDialog,
+    QDialogButtonBox,
+    QVBoxLayout, QLabel,
+    QMainWindow,
+    QPushButton,
+)
+
+class CustomDlg(QDialog): 
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle('Hello, QDialog')
+
+        buttons = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        # buttons에 해당하는 button객체
+        self.button_box = QDialogButtonBox(buttons) 
+
+        #QDialog의 메서드를 slot으로
+        self.button_box.accepted.connect(self.accept) 
+        #QDialog의 메서드를 slot으로
+        self.button_box.rejected.connect(self.reject)    
+
+        self.layout = QVBoxLayout()
+        message = QLabel('Is something ok?')
+        self.layout.addWidget(message)
+        # QDialogButtonBox객체 추가.
+        self.layout.addWidget(self.button_box) 
+        self.setLayout(self.layout)
+
+class MW(QMainWindow): 
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QDialog Ex.")
+
+        button = QPushButton("Press it for a Dialog")
+        button.clicked.connect(self.button_clicked)
+
+        self.setCentralWidget(button)
+
+    def button_clicked(self, s):
+        print("click", s)
+        
+        # -------------
+        # for custom dlg
+        dlg = CustomDlg(self)
+        if dlg.exec(): # Modal Dialog
+            print('ok')
+        else:
+            print("cancel")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MW()
+    window.show()
+    app.exec()
